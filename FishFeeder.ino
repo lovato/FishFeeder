946db68d7c88c5654427cd85a88a6f54d329fdcd
Marco Lovato <maglovato@gmail.com>
2013-12-11 00:36:39 -0200

    initial release

---
+++ FishFeeder.ino
@@ -0,0 +1,107 @@
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+// This example code is in the public domain.
+
+
+#include <Servo.h> 
+ 
+Servo servoPorta;
+Servo servoPote;
+ 
+int pos = 0;
+boolean doloop = true;
+int pos_jogar_comida = 58;
+int porta_aberta = 145;
+int porta_fechada = 0;
+int estacionamento = 58;
+
+void setup() 
+{ 
+  servoPote.attach(9);
+  servoPorta.attach(8);
+} 
+ 
+ 
+void loop() 
+{ 
+  if (doloop)
+  {
+    inicializacao();
+    restaurante_fish();
+    despeja_comida();
+    estacionar();
+  }
+  doloop = false;
+}
+
+void inicializacao()
+{
+  fecha_porta();
+  move_motor(servoPote, 179);
+  shake(servoPote);
+  shake(servoPote);
+  shake(servoPote);
+}
+
+void despeja_comida()
+{
+  move_motor(servoPote, pos_jogar_comida);
+  abre_porta();
+  fecha_porta();
+}
+
+void restaurante_fish()
+{
+  //funcao que abastece o mini reservatorio com comida
+  move_motor(servoPote, 1);
+  shake(servoPote);
+  shake(servoPote);
+}
+
+void abre_porta()
+{
+  move_motor(servoPorta, porta_aberta);
+}
+
+void fecha_porta()
+{
+  move_motor(servoPorta, porta_fechada);
+}
+
+void estacionar()
+{
+  // funcao para fazer o pote ficar em posicao de reabastecimento
+  move_motor(servoPote, estacionamento); 
+}
+
+void move_motor(Servo srv, int pos)
+{
+  srv.write(pos);
+  delay(1000);
+}
+
+void shake(Servo srv)
+{
+  int originalPos = srv.read();
+
+  int pos = originalPos;
+  int i = 0;
+  int force = 20;
+
+  if (pos<force) {
+    pos = force;
+  }
+  if (pos>(180-force)) {
+    pos = (180-force);
+  }
+
+  for(i = 0; i < 30; i += 1)
+  {
+    srv.write(pos + force);
+    delay(15);
+    srv.write(pos - force);
+    delay(15);
+  }
+  srv.write(originalPos);
+  delay(15);
+}
